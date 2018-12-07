579e8a76a5bc513a6bfa861dbeaabe8f18f30f18
Brad Cannell <brad.cannell@gmail.com>
2018-12-06 19:38:41 -0600

    Moved codebook_get_df_attributes functionality into codebook
    Pulled all of the code out of codebook_get_df_attributes and put it directly inside the codebook function. This was primarily done so that the dataset attributes table actual data frame would contain the name of the actual data frame instead of “df.”

--- R/codebook.R
+++ R/codebook.R
@@ -30,6 +30,38 @@
 codebook <- function(df, path = NA, title = NA, subtitle = NA, description = NA) {
 
   # ===========================================================================
+  # Variable management
+  # ===========================================================================
+
+
+  # ===========================================================================
+  # Checks
+  # ===========================================================================
+  # Check to make sure df is a data frame
+  if ( !("data.frame" %in% class(df)) ) {
+    stop("Expecting df to be of class data.frame. Instead it was ", class(df))
+  }
+
+  # Check to make sure the user is not piping in the dataset name
+  df_name <- deparse(substitute(df))
+  if (df_name == ".") {
+    message("The function get_df_attributes is seeing '.' as the df name. ",
+            "This can be caused by piping df into the get_df_attributes fucntion.")
+  }
+
+  # Check for file path
+  if (is.na(path)) {
+    stop("Codebook expects that df is a data frame that you have read ",
+         "into memory from a saved data file. Please provide the path ",
+         "to the saved data file.")
+  }
+
+  # Check that file path is valid
+  if (!file.exists(path)) {
+    stop("The argument to 'path' is not a valid file path.")
+  }
+
+  # ===========================================================================
   # Create an empty Word rdocx object
   # default template contains only an empty paragraph
   # Using cursor_begin and body_remove, we can delete it
@@ -48,16 +80,77 @@ codebook <- function(df, path = NA, title = NA, subtitle = NA, description = NA)
   )
 
   # ===========================================================================
+  # Copy code from utils:::format.object_size
+  # Can't use ::: operator on CRAN
+  # ===========================================================================
+  format_object_size <- function (x, units = "b", standard = "auto", digits = 1L, ...) {
+    known_bases <- c(legacy = 1024, IEC = 1024, SI = 1000)
+    known_units <- list(
+      SI = c("B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"),
+      IEC = c("B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"),
+      legacy = c("b", "Kb", "Mb", "Gb", "Tb", "Pb"),
+      LEGACY = c("B", "KB", "MB", "GB", "TB", "PB")
+    )
+    units <- match.arg(units, c("auto", unique(unlist(known_units), use.names = FALSE)))
+    standard <- match.arg(standard, c("auto", names(known_bases)))
+
+    if (standard == "auto") {
+      standard <- "legacy"
+      if (units != "auto") {
+        if (grepl("iB$", units)) {
+          standard <- "IEC"
+        } else if (grepl("b$", units)) {
+          standard <- "legacy"
+        } else if (units == "kB") {
+          stop("For SI units, specify 'standard = \"SI\"'")
+        }
+      }
+    }
+
+    base <- known_bases[[standard]]
+    units_map <- known_units[[standard]]
+
+    if (units == "auto") {
+      power <- if (x <= 0) {
+        0L
+      } else {
+        min(as.integer(log(x, base = base)), length(units_map) - 1L)
+      }
+    } else {
+      power <- match(toupper(units), toupper(units_map)) - 1L
+      if (is.na(power)) {
+        stop(gettextf(
+          "Unit \"%s\" is not part of standard \"%s\"",
+          sQuote(units), sQuote(standard)), domain = NA
+        )
+      }
+    }
+
+    unit <- units_map[power + 1L]
+    if (power == 0 && standard == "legacy") {
+      unit <- "bytes"
+    }
+    paste(round(x/base^power, digits = digits), unit)
+  }
+
+  # ===========================================================================
   # Add metadata to codebook shell
   # ===========================================================================
-  # Get metadata
-  df_metadata <- codebook_get_df_attributes(df, path = path) %>%
-    flextable::regulartable() %>%
-    codebook_theme_df_attributes()
+  # Create tibble of metadata
+  meta <- tibble(
+    `Dataset name:` = df_name,
+    `Dataset size:` = df %>% object.size() %>% format_object_size(units = "auto"),
+    `Column count:` = df %>% ncol() %>% format(big.mark = ","),
+    `Row count:` = df %>% nrow() %>% format(big.mark = ","),
+    `Last modified date:` = file.mtime(path) %>% as.character()
+  ) %>%
+    gather() %>%  # Reorient the data frame vertically
+    flextable::regulartable() %>% # Convert to flextable
+    codebook_theme_df_attributes() # Format
 
   # Add metadata to codebook
   rdocx <- rdocx %>%
-    flextable::body_add_flextable(df_metadata)
+    flextable::body_add_flextable(meta)
 
   # ===========================================================================
   # Optionally Add dataset description
